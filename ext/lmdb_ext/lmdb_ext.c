6e9f5a0808b5ccefb6a9b451236484b253dcb531
Dorian Taylor <code@doriantaylor.com>
2024-03-02 12:52:36 -0500

    stop ruby >=3.2 from complaining

--- ext/lmdb_ext/lmdb_ext.c
+++ ext/lmdb_ext/lmdb_ext.c
@@ -297,8 +297,6 @@ static VALUE with_transaction(VALUE venv, VALUE(*fn)(VALUE), VALUE arg, int flag
     if (vparent && !NIL_P(vparent))
         Data_Get_Struct(vparent, Transaction, tparent);
 
-    // rb_warn("fart lol");
-
     // XXX note this is a cursed goto loop that could almost certainly
     // be rewritten as a do-while
  retry:
@@ -1572,7 +1570,8 @@ void Init_lmdb_ext() {
          * A general class of exceptions raised within the LMDB gem.
          */
         cError = rb_define_class_under(mLMDB, "Error", rb_eRuntimeError);
-#define ERROR(name) cError_##name = rb_define_class_under(cError, #name, cError);
+#define ERROR(name) \
+        cError_##name = rb_define_class_under(cError, #name, cError);
 #include "errors.h"
 #undef ERROR
 
@@ -1601,6 +1600,7 @@ void Init_lmdb_ext() {
          *    env.close
          */
         cEnvironment = rb_define_class_under(mLMDB, "Environment", rb_cObject);
+        rb_undef_alloc_func(cEnvironment);
         rb_define_singleton_method(cEnvironment, "new", environment_new, -1);
         rb_define_method(cEnvironment, "database", environment_database, -1);
         rb_define_method(cEnvironment, "active_txn", environment_active_txn, 0);
@@ -1642,6 +1642,7 @@ void Init_lmdb_ext() {
          *    env.close
          */
         cDatabase = rb_define_class_under(mLMDB, "Database", rb_cObject);
+        rb_undef_alloc_func(cDatabase);
         rb_undef_method(rb_singleton_class(cDatabase), "new");
         rb_define_method(cDatabase, "stat", database_stat, 0);
         rb_define_method(cDatabase, "flags", database_get_flags, 0);
@@ -1713,6 +1714,7 @@ void Init_lmdb_ext() {
          *                            #=> storage.
          */
         cTransaction = rb_define_class_under(mLMDB, "Transaction", rb_cObject);
+        rb_undef_alloc_func(cTransaction);
         rb_undef_method(rb_singleton_class(cTransaction), "new");
         rb_define_method(cTransaction, "commit", transaction_commit, 0);
         rb_define_method(cTransaction, "abort", transaction_abort, 0);
@@ -1746,6 +1748,7 @@ void Init_lmdb_ext() {
          *    end
          */
         cCursor = rb_define_class_under(mLMDB, "Cursor", rb_cObject);
+        rb_undef_alloc_func(cCursor);
         rb_undef_method(rb_singleton_class(cCursor), "new");
         rb_define_method(cCursor, "close", cursor_close, 0);
         rb_define_method(cCursor, "get", cursor_get, 0);
